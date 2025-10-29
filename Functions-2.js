13883e5119ae037cb5a9115900b7141ecec43382
Alexis28J <jordy2806@hotmail.com>
2025-10-29 12:01:46 +0100

    Esercizi (29.10.2025)

--- Functions-2.js
+++ Functions-2.js
@@ -161,23 +161,23 @@
 //6) rimuovere tutte le p
 
 // function strangeStringFunction(str, multiplier) {
-    
+
 //     const strWithoutSpaces = str.trim();   // funzione .trim() toglie lo spazio in mezzo alla stringa (1)
 //     console.log('senza spazi', strWithoutSpaces);  //così controllo che ogni task che ho fatto funzioni bene
 
 //     const strWithPippo = strWithoutSpaces + 'pippo';  // per aggiungere pippo in fondo (2)
 //     console.log('aggiunto pippo', strWithPippo);
-    
+
 //     const strUpper = strWithPippo.toUpperCase();  //funzione .toUpperCase() per mettere tutto in maiuscola (3)
 //     console.log('maiuscola', strUpper);
 
 //     const strMultiplied = strUpper.repeat(multiplier);
 //     console.log('moltiplicata->', strMultiplied);
-    
+
 //     const strWithLength = strMultiplied + strMultiplied.length;
 //     console.log('con lunghezza->', strWithLength);
-    
-    
+
+
 // let strWithoutP = '';                              //(6)
 
 // for (let i = 0; i < strWithLength; i++) {
@@ -186,7 +186,7 @@
 //     if(char !== 'P'){
 //         strWithoutP += char;
 //     }
-    
+
 // }
 
 // console.log('senza p->', strWithoutP);
@@ -219,6 +219,79 @@
 
 // Successivamente, scrivi una funzione chiamata countChar che si comporti come countBs, tranne per il fatto che accetta un secondo argomento che indica il carattere da contare (invece di contare solo i caratteri B maiuscoli). Riscrivi countBs per utilizzare questa nuova funzione.
 
+//Fatto da Selma
+
+// function countBs(str) {
+//     let counter = 0;
+
+//     for (let i = 0; i < str.length; i++) {  //ricorda che si comincia a contare da ZERO non da uno
+//         const selectedChar = str[i];
+//         const lowerChar = selectedChar.toLowerCase(); //funzione .toLowerCase() per trasformare le lettere maiscole in minuscole perché altrimenti la nostra funzione non lo considera 
+
+//         if (selectedChar === 'b') {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+// console.log(countBs('babbeo'));  // -> 3  //6 caratteri 
+// console.log(countBs('leonardo')); // -> 0 //8 caratteri
+// console.log(countBs('BAMBINO')); // -> 2  //7 caratteri
+
+
+
+
+
+// function countChar(str, char) {
+//      let counter = 0;
+
+//     for (let i = 0; i < str.length; i++) {
+//         const selectedChar = str[i];  //variabile i è la posizione dentro del ciclo
+
+//         const selectedLower = selectedChar.toLowerCase(); //funzione .toLowerCase() per trasformare le lettere maiscole in minuscole perché altrimenti la nostra funzione non lo considera 
+//         const charLower = char.toLowerCase(); //dobbiamo aggiungere
+
+//         if (selectedChar === charLower) {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+// console.log(countChar('mammamia', 'm')); // -> 4
+// console.log(countChar('mammamia', 'i')); // -> 1
+// console.log(countChar('leonardo', 'L')); // -> 1 ma la console, ci torna 0 perciò *
+
+
+
+//TASK AGGIUNTIVO - is Sensitive
+
+// function countCharSensitive(str, char, isSensitive) {
+
+//     let counter = 0;
+
+//     for (let i = 0; i < str.length; i++) {
+
+//         let selectedChar = str[i];
+
+//         if (!isSensitive) {
+//             selectedChar = selectedChar.toLowerCase();
+//             char = char.toLowerCase();
+//         }
+//         if (selectedChar === char) {
+//             counter++;
+//         }
+//     }
+
+//     return counter;
+// }
+
+// console.log(countCharSensitive('leonardo', 'L', false));  // -> 1
+// console.log(countCharSensitive('leonardo', 'L', true)); // -> 0
+
+
+
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -228,6 +301,7 @@
 //24) Inversione di un numero
 //Scrivi una funzione invertiNumero che prenda un numero come parametro e restituisca il numero con le cifre invertite (es. 123 → 321).
 
+//fatto con Evelyn :)
 // function changeCharOrder(n) {
 //     let toChange = n.toString().split('').reverse().join('');
 //     return toChange;
@@ -236,34 +310,164 @@
 // console.log(changeCharOrder(598));
 
 
+//fatto dal Prof.
+
+// function reverseNumber(nbr) {   //La forma più furba è transformarlo in stringa
+
+//     const nbrToString = String(nbr);
+
+//     let reversed = '';
+
+//     //le stringhe si possono 'ciclare' - ma facciamo un for al CONTRARIO
+
+//     for (let i = nbrToString.length - 1; i >= 0 ; i--) {
+
+//         const char = nbrToString[i];
+//         reversed += char;
+
+//     }
+
+//     const reversedToNumber = Number(reversed); //devo riconvertirlo in numero o mi ritornerà come stringa (ERRORE)
+//     return reversedToNumber;
+
+// }
+
+// console.log(reverseNumber(123345));  
+
 
 //25) Tabellina
 //Scrivi una funzione tabellina che prenda un numero come parametro e stampi in console la tabellina di quel numero fino a 10.
 
 
+//fatto con Evelyn :)
 // function littleTab(n1) {
 
 
 // for (let i = 1; i <= 10; i++) {
-  
+
 //   const risultato = n1 * i;
-  
+
 //   console.log(n1 + 'x' + i + '=' + risultato);
 // }
 
 // }
 
-// console.log(littleTab(5));
+// console.log(littleTab(5));  
+
+
+//fatto dal Prof.
+
+//MODO PIù BRUTTO DA FARLO MA NULLA CI VIETA DI FARLO (è lecito) :)
+// function tabellina(nbr) {
+//     console.log(nbr * 1);
+//     console.log(nbr * 2);
+//     console.log(nbr * 3);
+//     console.log(nbr * 4);
+//     console.log(nbr * 5);
+//     console.log(nbr * 6);
+//     console.log(nbr * 7);
+//     console.log(nbr * 8);
+//     console.log(nbr * 9);
+//     console.log(nbr * 10);
+
+// }
+
+// tabellina(3);
+// tabellina(10);
+
+
+//MODO 'CORRETTO'
+
+// function tabellinaEvolution(nbr) {
+//     for (let i = 0; i < 11; i++) {
+//         console.log(nbr * i);  
+//     }
+// }
+
+// tabellinaEvolution(3); //  NON SERVE FARE IL CONSOLE.LOG OGNI VOLTA SE è GIà STATO FATTO PRIMA
+// tabellinaEvolution(10); //  NON SERVE FARE IL CONSOLE.LOG OGNI VOLTA SE è GIà STATO FATTO PRIMA
+
+
+// UNA TABELLA STRING (ALTRO ESERCIZIO)
+
+// function tabellinaString(nbr) {
+
+//     let tabellinaStr = '';
+
+//     for (let i = 1; i < 11; i++) {
+//         const result = nbr * i; 
+//         tabellinaStr += result;
+//         tabellinaStr += '';  //perché le parole non stiano appiccicate
+
+//     }
+
+//     return tabellinaStr;
+// }
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(tabellinaString(i));
+// }
 
 
 //26) Fibonacci
 //Scrivi una funzione fibonacci che prenda un numero N come parametro e restituisca l’N-esimo numero della sequenza di Fibonacci.
 
+//fatto dal Prof.
+
+// function fibonacci(pos) {   //pos sta per position
+
+// let first = 0;
+// let second= 1;
+
+// if (pos === 1) {
+//     return first;
+// }else if(pos === 2){
+//     return second;
+// }else if(pos > 2){
+
+// let fib;
+
+//     for (let i = 3; i <= pos; i++) {  //si comincia dalla posizione 3
+//         fib = first + second;   //la terza posizione = la prima + il secondo
+//         first = second;
+//         second = fib;
+//     }
+
+//      return fib;
+
+// }else{
+//     console.log('ERRORE!!!');  // in caso si inserisca un parametro negativo
+
+// }
+
+// }
+
+// console.log(fibonacci(7));
+
+
+//FUNZIONE RICORSIVA (VERSIONE RICORSIVA - è più 'elegante')
+
+function fiboRecursive(pos) {
+
+    if (pos === 0) {
+        return 0;
+    } else if (pos === 1) {
+        return 1;
+    } else {
+        return fiboRecursive(pos - 1) + fiboRecursive(pos - 2);  //se metto 3: fiboRecursive(2) + fiboRecursive(1)
+    }
+}
+
+
+
+
+
+
 
 //27)  Conteggio vocali
 //Scrivi una funzione contaVocali che prenda una stringa come parametro e restituisca il numero di vocali presenti.
 
-
+//fatto con Evelyn :)
 // function contaVocali(str) {
 //     let counter = 0;
 //     const vowel = 'aeiouAEIOU';
@@ -271,7 +475,7 @@
 //         if (vowel.includes(str[i])) {
 //             counter++;
 //         }
-        
+
 //     }
 //     return counter;
 // }
@@ -280,11 +484,36 @@
 // console.log(contaVocali('meleebanane'));
 
 
+//fatto dal Prof.
+
+// function countVowels(str) {
+//     let counter = 0;
+
+//     for (let i = 0; i < str.length; i++) {  //ricorda che si comincia a contare da ZERO non da uno
+
+//         const selectedChar = str[i];
+//         const lowerChar = selectedChar.toLowerCase(); //funzione .toLowerCase() per trasformare le lettere maiscole in minuscole perché altrimenti la nostra funzione non lo considera 
+
+//         if (lowerChar === 'a' 
+//             || lowerChar === 'e'
+//             || lowerChar === 'i'
+//             || lowerChar === 'o'
+//             || lowerChar === 'u'
+//             ) {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+// console.log(countVowels('aiuola'));  // -> 5 vocali
+
+
 
 //28) Sconto
 //Scrivi una funzione applicaSconto che prenda due numeri come parametri (prezzo e percentuale di sconto) e restituisca il prezzo scontato.
 
-
+//fatto con Evelyn :)
 // function applicaSconto(price, percentage) {
 //     let percentageFloat = percentage / 100;
 //     let discount = price * percentageFloat;
@@ -292,15 +521,28 @@
 //     return result;
 // }
 
-
 // console.log(applicaSconto(100, 5));
 
+
+
+//fatto dal Prof.
+
+// function applySale(price, salePercent) {
+
+//     const sale = price / 100 * salePercent;
+//     const priceWithSale = price - sale;
+
+//     return priceWithSale;
+// }
+
+// console.log(applySale(100, 20));
 
 
 //29) Conversione gradi
 //Scrivi una funzione convertiGradi che prenda un numero come parametro (gradi Celsius) e restituisca la conversione in Fahrenheit
 
- 	
+
+//fatto con Evelyn :)
 //(5 °C × 9/5) + 32 = 41 °F
 
 // function convertiGradi(c) {
@@ -311,10 +553,55 @@
 // console.log(convertiGradi(5) + '°F') ;
 
 
+//fatto dal Prof. 
+//PRATICAMENTE UGUALE
+
+// function celsiusToFahrenheit(c) {
+//     const f = (c * (9/5)) + 32;
+//     return f;
+// }
+
+// console.log(celsiusToFahrenheit(30));
+
+
 //30) Verifica password
 //Scrivi una funzione verificaPassword che prenda una stringa e restituisca true se:
 // - Ha almeno 8 caratteri
 // - Contiene almeno una lettera maiuscola
 // - Contiene almeno un carattere speciale tra questi '!#@$%'
 // - Non contiene la parola 'cacca'
+
+//fatto dal Prof.
+
+
+function checkPasword(password) {
+    if (password.length > 8) {  //la password deve almeno avere 8 caratteri
+        return false;
+    } else {
+        if (password.toLowerCase() === password) {
+            return false;  //la password deve avere almeno UNA lettera maiuscola
+        } else {
+            if (password.includes("!")   //la password deve avero almeno UNO di questi caratteri speciali
+                || password.includes('#')
+                || password.includes('@')
+                || password.includes('$')
+                || password.includes('%')) {
+
+                if (password.includes('cacca')) {  //la password NON deve includere la parola 'cacca'
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+}
+
+
+
+console.log(checkPasword('caccacaccacacca'));
+console.log(checkPasword('leonardo'));
 
